923a098a683bc8e7086050893ca5b3ed31fb4f2c
Linh Vo <hailinh.vo@gmail.com>
2013-04-24 14:26:40 -0500

    new app create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartEating::Application.config.secret_token = '50a66edc8d5ea2ae8c69a27087eb7b8ca16d9405f3b307e71d2b2ae0e0ce2b7dd6d6ed5a42caa201adc004d77f9da76d127671654aeb425bb622dd4ff16d409c'
